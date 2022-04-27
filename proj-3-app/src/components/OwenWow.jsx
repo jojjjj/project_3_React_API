04f4215ab543ba3df1a5e23022926de166ac5522
Joelle Barr <joellebarr22@gmail.com>
2022-04-26 22:58:11 -0400

    adding some css

--- proj-3-app/src/components/OwenWow.jsx
+++ proj-3-app/src/components/OwenWow.jsx
@@ -3,12 +3,14 @@ import axios from 'axios'
 
 const OwenWow = (displayWow) => {
     const [wow, setWow] = useState('')
+    const [wowDetails, setWowDetails] = useState('')
 
     useEffect(() => {
       const getOwenWow = async () => {
         const res = await axios.get("https://owen-wilson-wow-api.herokuapp.com/wows/random")
             console.log(res)
             setWow(res.data[0].video["480p"])
+            setWowDetails(res.data)
       }
       getOwenWow()
     }, [displayWow])
@@ -28,15 +30,17 @@ const OwenWow = (displayWow) => {
     // }, [])
 
     return (
+      <div className="details">
         <iframe
           // width="500"
-          height="600"
+          // height="400"
           src={wow}
           title="Owen Wilson Wow Video"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>
+      </div>
     )
   }
   
